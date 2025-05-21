2ea7000045aee1b1af398cde665ceede310a776d
punp2002 <punp2002@gmail.com>
2025-05-21 11:08:51 +0900

    react2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Exam1 from './components/Exam1';
 import Exam2 from './components/Exam2';
+import Exam3 from './components/Exam3';
+import Exam4 from './components/Exam4';
+import Exam5 from './components/Exam5';
 
 
 function App() {
@@ -20,12 +23,15 @@ function App() {
   </>
   */
 
-    <>
-      <button onClick={() => setShowExam(!showExam)}>클릭</button>
+    // <>
+    //   <button onClick={() => setShowExam(!showExam)}>클릭</button>
 
-      {/* showExam이 true면 화면에 Exam1 컴포넌트 호출하여 렌더링 함 */}
-      { showExam && <Exam2 jaesuk="hello" test="world"/>}      
-    </>    
+    //   {/* showExam이 true면 화면에 Exam1 컴포넌트 호출하여 렌더링 함 */}
+    //   { showExam && <Exam2 jaesuk="hello" test="world"/>}      
+    // </>    
+    //<Exam3 />
+    //<Exam4 />
+    <Exam5 />
   );
 }
 
